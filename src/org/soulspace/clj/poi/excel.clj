1d5ead743c0bad4ffb2a6eb057760fffab37e704
Ludger Solbach <soulman@soulspace.org>
2013-06-17 08:58:41 +0200

    enhancements, refactorings

--- src/org/soulspace/clj/poi/excel.clj
+++ src/org/soulspace/clj/poi/excel.clj
@@ -2,7 +2,7 @@
   (:use [org.soulspace.clj.java beans])
   (:import [org.apache.poi.poifs Filesystem]
            [org.apache.poi.ss.util CellRangeAddress]
-           [org.apache.poi.ss.usermodel Cell CellStyle DataFormat Font Row Sheet Workbook WorkbookFactory]
+           [org.apache.poi.ss.usermodel Cell CellReference CellStyle DataFormat Font Row Sheet Workbook WorkbookFactory]
            [org.apache.poi.hssf.usermodel HSSFWorkbook]
            [org.apache.poi.xssf.usermodel XSSFWorkbook]))
 
@@ -18,11 +18,11 @@
   (set-properties! c args)
   c)
 
-(defn create-workbook
-  ([]
-    HSSFWorkbook.)
-  ([input]
-  (WorkbookFactory/create input)))
+(defn workbook
+  ([args]
+    (init-model HSSFWorkbook. args))
+  ([input args]
+    (init-model (WorkbookFactory/create input) args)))
 
 (defn read-workbook [wb input]
   (.read wb input))
@@ -30,22 +30,22 @@
 (defn write-workbook [wb output]
   (.write wb output))
 
-(defn create-cell-style [wb args]
+(defn cell-style [wb args]
   (init-model (.createCellStyle wb) args))
 
-(defn create-font [wb args]
+(defn font [wb args]
   (init-model (.createFont wb) args))
 
-(defn create-data-format [wb args]
+(defn data-format [wb args]
   (init-model (.createDataFormat wb) args))
 
-(defn create-sheet [wb sheet-no args]
+(defn sheet [wb sheet-no args]
   (init-model (.createSheet wb sheet-no) args))
 
 (defn get-sheet [wb sheet-no]
   (.getSheetAt wb sheet-no))
 
-(defn create-row [sheet row-no args]
+(defn row [sheet row-no args]
   (init-model (.createRow sheet row-no) args))
 
 (defn get-row [sheet row-no]
@@ -54,7 +54,7 @@
 (defn physical-number-of-rows [sheet]
   (.getPhysicalNumberOfRows sheet))
 
-(defn create-cell [row cell-no args]
+(defn cell [row cell-no args]
   (init-model (.createCell row cell-no) args))
 
 (defn get-cell [row cell-no]
@@ -63,14 +63,37 @@
 (defn physical-number-of-cells [row]
   (.getPhysicalNumberOfCell row))
 
-(defn set-cell-value [cell]
-  )
+(defn set-cell-value [cell value]
+  (.setCellValue cell value))
 
-(defn get-cell-value [cell]
-  )
+(defmulti get-cell-value get-cell-type)
+
+(defmethod get-cell-value (cell-type :numeric)
+  [cell]
+  (if (.isCellDateFormatted cell)
+    (.getDateCellValue cell)
+    (.getNumericCellValue cell)))
+
+(defmethod get-cell-value (cell-type :string)
+  [cell]
+  (.getString (.getRichStringCellValue cell)))
+
+(defmethod get-cell-value (cell-type :formula)
+  [cell]
+  (.getCellFormula cell))
+
+(defmethod get-cell-value (cell-type :boolean)
+  [cell]
+    (.getBooleanCellValue cell))
+
+(defmethod get-cell-value (cell-type :blank)
+  [cell])
+
+(defmethod get-cell-value (cell-type :error)
+  [cell])
 
 (defn get-cell-type [cell]
-  )
+  (.getCellType cell))
 
 (defn set-cell-type [cell type]
   (.setCellType cell type))
