509008eb1616b91a840d41a357c39fd285f1044c
Ludger Solbach <soulman@soulspace.org>
2013-07-30 20:43:53 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -1,20 +1,20 @@
 [
- :name "CljPoiLibrary"
+ :module "CljPoiLibrary"
  :project "org.soulspace.clj"
- :type "library"
+ :type :library
  :version "0.1.0"
  :description "An Apache POI library wrapper in clojure"
- :plugins ["global" "deps" "clojure" "package"]
- :dependencies [["org.clojure" "clojure" "1.3.0"]
-                ["org.soulspace.clj" "CljLibrary" "0.3.0"]
-                ["org.soulspace.clj" "CljJavaLibrary" "0.2.0"]
-                ["org.apache.poi" "poi" "3.9"]
-                ["org.apache.poi" "poi-ooxml" "3.9"]
-                ["org.apache.poi" "poi-ooxml-schemas" "3.9"]
-                ["org.apache.xmlbeans" "xmlbeans" "2.5.0"]
-                ["dom4j" "dom4j" "1.6.1"]
-                ["stax" "stax-api" "1.0.1"]
-                ["commons-codec" "commons-codec" "1.5"]
-                ["commons-logging" "commons-logging" "1.1.1"]
-                ["log4j" "log4j" "1.2.15"]]
+ :plugins ["global" "sdeps" "clojure" "package"]
+ :dependencies [[["org.clojure" "clojure" "1.5.1"]]
+                [["org.soulspace.clj" "CljLibrary" "0.3.0"]]
+                [["org.soulspace.clj" "CljJavaLibrary" "0.2.0"]]
+                [["org.apache.poi" "poi" "3.9"]]
+                [["org.apache.poi" "poi-ooxml" "3.9"]]
+                [["org.apache.poi" "poi-ooxml-schemas" "3.9"]]
+                [["org.apache.xmlbeans" "xmlbeans" "2.5.0"]]
+                [["dom4j" "dom4j" "1.6.1"]]
+                [["stax" "stax-api" "1.0.1"]]
+                [["commons-codec" "commons-codec" "1.5"]]
+                [["commons-logging" "commons-logging" "1.1.1"]]
+                [["log4j" "log4j" "1.2.15"]]]
  ]
