d60ef8702d55d031024069a6cd9d7d6731d468e5
Ludger Solbach <soulman@soulspace.org>
2013-07-31 10:07:52 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,8 +4,7 @@
  :type :library
  :version "0.1.0"
  :description "A library for the integration of JFreechart in clojure"
- :plugins ["global" "sdeps" "clojure" "package"]
+ :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
-                [["jfree" "jcommon" "1.0.16"]]
                 [["jfree" "jfreechart" "1.0.13"]]]
  ]
