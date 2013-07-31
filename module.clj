a5050bcef36b5fd5627327a4e2d1d82ddf711c6c
Ludger Solbach <soulman@soulspace.org>
2013-07-31 09:58:14 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,12 +4,9 @@
  :type :library
  :version "0.1.0"
  :description "Clojure XSLT library"
- :plugins ["global" "sdeps" "clojure" "package"]
+ :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
                 [["xalan" "xalan" "2.7.1"]] ; TODO check version
                 [["xalan" "serializer" "2.7.1"]] ; TODO check version, check transitive
-                [["xerces" "xercesImpl" "2.9.1"]] ; transitive 
-                [["xml-apis" "xml-apis" "1.3.04"]] ; transitive 
-                [["xml-apis" "xml-apis-ext" "1.3.04"]] ; transitive 
                 ]
  ]
