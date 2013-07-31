59bc3bbf3222ebcb0fc581555a5288f1b48eb960
Ludger Solbach <soulman@soulspace.org>
2013-07-31 10:08:06 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,17 +4,9 @@
  :type :library
  :version "0.1.0"
  :description "An Apache POI library wrapper in clojure"
- :plugins ["global" "sdeps" "clojure" "package"]
+ :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
-                [["org.soulspace.clj" "CljLibrary" "0.3.0"]]
                 [["org.soulspace.clj" "CljJavaLibrary" "0.2.0"]]
-                [["org.apache.poi" "poi" "3.9"]]
                 [["org.apache.poi" "poi-ooxml" "3.9"]]
-                [["org.apache.poi" "poi-ooxml-schemas" "3.9"]]
-                [["org.apache.xmlbeans" "xmlbeans" "2.5.0"]]
-                [["dom4j" "dom4j" "1.6.1"]]
-                [["stax" "stax-api" "1.0.1"]]
-                [["commons-codec" "commons-codec" "1.5"]]
-                [["commons-logging" "commons-logging" "1.1.1"]]
-                [["log4j" "log4j" "1.2.15"]]]
+                ]
  ]
