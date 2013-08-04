0acc136331582877a423cdf854a444d94c98325d
Ludger Solbach <soulman@soulspace.org>
2013-08-05 00:51:05 +0200

    bumped version to 0.2.0

--- module.clj
+++ module.clj
@@ -2,7 +2,7 @@
  :module "CljJFreechartLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.1.0"
+ :version "0.2.0"
  :description "A library for the integration of JFreechart in clojure"
  :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
