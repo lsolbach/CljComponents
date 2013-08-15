163404b9f2b60be86c0981772b7ada67fdd0f99f
Ludger Solbach <soulman@soulspace.org>
2013-08-15 12:06:15 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -3,11 +3,10 @@
  :project "org.soulspace.clj"
  :type :library
  :version "0.1.0"
- :description "The CljLibrary is a library for clojure providing convenience functions with no other dependencies than clojure itself."
+ :description "The CljMarkdownLibrary contains functions to generate markdown documents from clojure."
  :project-lead "Ludger Solbach"
  :provider "soulspace.org"
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :description "CljLibrary is a library for clojure providing convenience functions with no other dependencies."
  :plugins ["global" "dependencies" "clojure" "clojuretest" "package" "release"]
  :dependencies [["org.clojure/clojure, 1.5.1"]]
  ]
