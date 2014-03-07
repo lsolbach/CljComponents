450bc8c151add1bb3dc008fd17764db9779ed77f
Ludger Solbach <soulman@soulspace.org>
2014-03-07 09:13:37 +0100

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,9 +4,12 @@
  :type :library
  :version "0.1.0"
  :description "Clojure wrapper library for Apache FOP."
- :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
- :dependencies [[["org.clojure" "clojure" "1.5.1"]]
-                [["org.soulspace.clj" "CljJavaLibrary" "0.2.0"]]
-                [["org.apache.xmlgraphics" "fop" "1.0"]]
-                ]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/ClojurePlugin"]
+           ["org.soulspace.baumeister/ClojureTestPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["org.clojure/clojure, 1.5.1"]
+                ["org.soulspace.clj/CljJavaLibrary, 0.5.0"]
+                ["org.apache.xmlgraphics/fop, 1.0"]]
  ]
