4ddaa081cf90a4e3ada4b07f0fd39074fae8ec45
Ludger Solbach <soulman@soulspace.org>
2014-03-07 09:08:53 +0100

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,9 +4,13 @@
  :type :library
  :version "0.1.0"
  :description "Clojure XSLT library"
- :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
- :dependencies [[["org.clojure" "clojure" "1.5.1"]]
-                [["xalan" "xalan" "2.7.1"]] ; TODO check version
-                [["xalan" "serializer" "2.7.1"]] ; TODO check version, check transitive
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/ClojurePlugin"]
+           ["org.soulspace.baumeister/ClojureTestPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["org.clojure" "clojure, 1.5.1"]
+                ["xalan" "xalan" "2.7.1"] ; TODO check version
+                ["xalan" "serializer" "2.7.1"] ; TODO check version, check transitive
                 ]
  ]
