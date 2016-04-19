707f1b13307df2ec740dfc4a9e726e7da9fc65ab
Ludger Solbach <soulman@soulspace.org>
2016-04-19 09:06:17 +0200

    bumped bumped version to 0.3.0 using clojure 1.8.0
    updated jfreechart to version 1.0.19

--- CljJFreechartLibrary/module.clj
+++ CljJFreechartLibrary/module.clj
@@ -2,13 +2,13 @@
  :module "CljJFreechartLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.2.1"
+ :version "0.3.0"
  :description "A library for the integration of JFreechart in clojure"
  :plugins ["global"
            ["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
- :dependencies [["org.clojure/clojure, 1.7.0"]
-                ["org.soulspace.clj/CljJavaLibrary, 0.6.1"]
-                ["jfree/jfreechart, 1.0.13"]]
+ :dependencies [["org.clojure/clojure, 1.8.0"]
+                ["org.soulspace.clj/CljJavaLibrary, 0.7.0"]
+                ["org.jfree/jfreechart, 1.0.19"]]
  ]
