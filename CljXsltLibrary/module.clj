8b1989e1b067e0879c4ae11cf7f6cb4994d32a8b
Ludger Solbach <soulman@soulspace.org>
2016-04-19 08:48:28 +0200

    bumped bumped version to 0.2.0 using clojure 1.8.0

--- CljXsltLibrary/module.clj
+++ CljXsltLibrary/module.clj
@@ -2,13 +2,13 @@
  :module "CljXsltLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.1.1"
+ :version "0.2.0"
  :description "Clojure XSLT library"
  :plugins ["global"
            ["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
- :dependencies [["org.clojure/clojure, 1.7.0"]
+ :dependencies [["org.clojure/clojure, 1.8.0"]
                 ["xalan/xalan, 2.7.1"] ; TODO check version
                 ["xalan/serializer, 2.7.1"] ; TODO check version, check transitive
                 ]
