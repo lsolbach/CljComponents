450c70ac04b9f09f64974f73b78162baa4f48f29
Ludger Solbach <soulman@soulspace.org>
2016-04-19 08:57:37 +0200

    bumped bumped version to 0.5.0 using clojure 1.8.0
    updated poi to version 3.14

--- CljPoiLibrary/module.clj
+++ CljPoiLibrary/module.clj
@@ -2,13 +2,13 @@
  :module "CljPoiLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.4.2"
+ :version "0.5.0"
  :description "An Apache POI library wrapper in clojure"
  :plugins ["global"
            ["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
- :dependencies [["org.clojure/clojure, 1.7.0"]
-                ["org.soulspace.clj/CljJavaLibrary, 0.6.1"]
-                ["org.apache.poi/poi-ooxml, 3.9"]]
+ :dependencies [["org.clojure/clojure, 1.8.0"]
+                ["org.soulspace.clj/CljJavaLibrary, 0.7.0"]
+                ["org.apache.poi/poi-ooxml, 3.14"]]
  ]
