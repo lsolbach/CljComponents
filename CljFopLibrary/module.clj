8f19356cc309a96575a48f8c155ba05c7c7ebb69
Ludger Solbach <soulman@soulspace.org>
2016-04-20 09:15:11 +0200

    bumped version to 0.2.0 using clojure 1.8.0
    updated fop to version 2.1
    factory has to be fixed yet

--- CljFopLibrary/module.clj
+++ CljFopLibrary/module.clj
@@ -2,13 +2,13 @@
  :module "CljFopLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.1.1"
+ :version "0.2.0"
  :description "Clojure wrapper library for Apache FOP."
  :plugins ["global"
            ["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
- :dependencies [["org.clojure/clojure, 1.7.0"]
-                ["org.soulspace.clj/CljJavaLibrary, 0.6.1"]
-                ["org.apache.xmlgraphics/fop, 1.0"]]
+ :dependencies [["org.clojure/clojure, 1.8.0"]
+                ["org.soulspace.clj/CljJavaLibrary, 0.7.0"]
+                ["org.apache.xmlgraphics/fop, 2.1"]]
  ]
