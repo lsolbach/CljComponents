3b7a1e5e17ec8df00306fabdde689c1e73d5bb63
Ludger Solbach <lsolbach@web.de>
2017-10-15 11:52:42 +0200

    initial import of CljExifLibrary

---
+++ CljExifLibrary/module.clj
@@ -0,0 +1,13 @@
+[
+ :module "CljExifLibrary"
+ :project "org.soulspace.clj"
+ :type :library
+ :version "0.1.0"
+ :description "Clojure XSLT library"
+ :plugins [["org.soulspace.baumeister/ClojurePlugin"]
+           ["org.soulspace.baumeister/ClojureTestPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["org.clojure/clojure, 1.8.0"]
+                ["com.drewnoakes/metadata-extractor, 2.10.1"]
+                ]
+ ]
