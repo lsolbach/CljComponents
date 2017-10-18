a5cf99d5f1994f9ce5cf344214c8be1fcedb79f9
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2017-10-18 18:42:28 +0200

    updated build files

--- CljMarkdownLibrary/module.clj
+++ CljMarkdownLibrary/module.clj
@@ -3,7 +3,7 @@
  :project "org.soulspace.clj"
  :type :library
  :version "0.2.0"
- :description "The CljMarkdownLibrary contains functions to generate markdown documents from clojure."
+ :description "The CljMarkdownLibrary contains functions to generate markdown documents from Clojure"
  :project-lead "Ludger Solbach"
  :provider "soulspace.org"
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
