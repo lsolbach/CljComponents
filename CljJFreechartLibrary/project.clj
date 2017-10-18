41c535f642c370f73e1187ab0050f4c0813eef3b
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2017-10-18 18:49:01 +0200

    updated build files

--- CljJFreechartLibrary/project.clj
+++ CljJFreechartLibrary/project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 ]
+                 [org.soulspace.clj/CljJavaLibrary "0.7.0"]
+                 [org.jfree/jfreechart "1.0.19"]]
   :test-paths ["unittest"])
