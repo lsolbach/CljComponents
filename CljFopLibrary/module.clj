ed0f013d396ac355f390b3ae33ab0a6dbf9bf280
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2017-11-20 15:13:46 +0100

    updated fop version to 2.2, bumped version to 0.2.1

--- CljFopLibrary/module.clj
+++ CljFopLibrary/module.clj
@@ -2,7 +2,7 @@
  :module "CljFopLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.2.0"
+ :version "0.2.1"
  :description "The CljFopLibrary is a Clojure wrapper library for Apache FOP"
  :scm-url "https://github.com/lsolbach/CljComponents"
  :plugins [["org.soulspace.baumeister/ClojurePlugin"]
@@ -10,5 +10,5 @@
            ["org.soulspace.baumeister/PackagePlugin"]]
  :dependencies [["org.clojure/clojure, 1.8.0"]
                 ["org.soulspace.clj/CljJavaLibrary, 0.7.0"]
-                ["org.apache.xmlgraphics/fop, 2.1"]]
+                ["org.apache.xmlgraphics/fop, 2.2"]]
  ]
