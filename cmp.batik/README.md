430b90bef5cbb0873720292d7dd094dc0e6ab03e
Ludger Solbach <soulman@soulspace.org>
2020-07-23 09:55:24 +0200

    added cmp.batik component

---
+++ cmp.batik/README.md
@@ -0,0 +1,22 @@
+cmp.batik
+=========
+The cmp.batik library provides a Clojure wrapper for the Apache Batik SVG component.
+
+Usage
+-----
+Leiningen
+[org.soulspace.clj/cmp.batik "0.2.0"]
+
+
+Copyright
+---------
+© 2011-2020 Ludger Solbach
+
+License
+-------
+[Eclipse Public License 1.0](http://www.eclipse.org/legal/epl-v10.html)
+
+Code Repository
+---------------
+[CljXML on GitHub](https://github.com/lsolbach/CljXML)
+
---
+++ cmp.batik/project.clj
@@ -0,0 +1,12 @@
+(defproject org.soulspace.clj/cmp.batik "0.2.0"
+  :description "The cmp.batik library provides a Clojure wrapper for the Apache Batik SVG component."
+  :url "https://github.com/lsolbach/CljXML"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.apache.xmlgraphics/batik-swing "1.9.1"]
+                 [org.apache.xmlgraphics/batik-svggen "1.9.1"]
+                 [org.apache.xmlgraphics/batik-svg-dom "1.9.1"]
+                 [org.soulspace.clj/xml.core "0.5.0"]
+                 [org.soulspace.clj/clj.base "0.8.0"]]
+  :test-paths ["unittest"])
