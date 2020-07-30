d8dcfa939e447b98c827016051149025e7b5a334
Ludger Solbach <soulman@soulspace.org>
2020-07-30 11:24:11 +0200

    updated project files, added change log and deps.edn

--- cmp.exif/project.clj
+++ cmp.exif/project.clj
@@ -1,7 +1,8 @@
 (defproject org.soulspace.clj/cmp.exif "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The cmp.exif is a library for reading EXIF tags on images"
+  :url "https://github.com/lsolbach/CljComponents"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.drewnoakes/metadata-extractor "2.14.0"]])
+                 [com.drewnoakes/metadata-extractor "2.14.0"]]
+  :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]])
