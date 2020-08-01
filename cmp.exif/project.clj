948c5a61255e2808fd53d388882f9d4794c03ac9
Ludger Solbach <soulman@soulspace.org>
2020-08-01 04:08:59 +0200

    use deps.edn and lein-tools-deps

--- cmp.exif/project.clj
+++ cmp.exif/project.clj
@@ -3,6 +3,10 @@
   :url "https://github.com/lsolbach/CljComponents"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.drewnoakes/metadata-extractor "2.14.0"]]
+  ; use deps.edn dependencies
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  ;  :dependencies [[org.clojure/clojure "1.10.1"]
+  ;                 [com.drewnoakes/metadata-extractor "2.14.0"]]
   :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]])
--- cmp.xalan/project.clj
+++ cmp.xalan/project.clj
@@ -3,7 +3,15 @@
   :url "https://github.com/lsolbach/CljComponents"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [xalan/xalan "2.7.1"] ; TODO check version
-                 [xalan/serializer "2.7.1"]] ; TODO check version, check transitive]
-  :test-paths ["unittest"])
+
+  ; use deps.edn dependencies
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+
+;  :dependencies [[org.clojure/clojure "1.10.1"]
+;                 [xalan/xalan "2.7.1"] ; TODO check version
+;                 [xalan/serializer "2.7.1"]] ; TODO check version, check transitive]
+
+  :test-paths ["test"])
+
